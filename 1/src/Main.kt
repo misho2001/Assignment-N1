11f5bbe9aac1c1ebd02358ba88dfc74d6da961b7
misho2001 <72156251+misho2001@users.noreply.github.com>
2022-03-09 17:17:48 +0400

    Add files via upload

---
+++ 1/src/Main.kt
@@ -0,0 +1,30 @@
+fun main(){
+    //1
+//    val array1 = arrayOf(1, 2, 3, 4, 5, 6, 7)
+//
+//    var b = 0
+//    var a =0
+//    for (i in array1.indices step 2){
+//        a=a+1
+//        b =b + array1[i]
+//    }
+//    var c = b/a
+//    println(c)
+
+    //2
+//    fun func(x: String){
+//
+//        var string1 = x.reversed()
+//        if (string1 == x){
+//            println(true)
+//        }else{
+//            println(false)
+//        }
+//
+//    }
+//    func ("ada")
+
+}
+
+
+
